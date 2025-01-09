ade75c8f0939f478839e5ca7f2bc738c196a76be
Xondamir0 <alimjanovxondamir01@gmail.com>
2025-01-09 23:01:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+{}
+const userName = prompt("Enter your name");
+const userLogin = +prompt("Iltimos login kiriting");
+const userPassword = +prompt("Iltimos parol kiriting");
+
+const mars = {
+  ism: userName,
+  login: userLogin,
+  password: userPassword,
+};
+
+console.log(mars);
